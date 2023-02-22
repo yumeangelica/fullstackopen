9b9e1e2216dc8dc0a031a77a363eeda6dde3d9b5
Angelica <angelica@Angelica-Macbook.wlan>
2023-02-22 18:39:37 +0200

    fix bug that sent too many requests to backend

--- osa7/ultimate-hooks/src/App.js
+++ osa7/ultimate-hooks/src/App.js
@@ -2,94 +2,198 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const useField = (type) => {
-    const [value, setValue] = useState('')
+  const [value, setValue] = useState('')
 
-    const onChange = (event) => { // asettaa inputin arvon stateen
-        setValue(event.target.value)
-    }
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
 
-    return { // palauttaa objektin, jossa on value, onChange ja type
-        type,
-        value,
-        onChange
-    }
+  return {
+    type,
+    value,
+    onChange
+  }
 }
 
-// custom hook, joka palauttaa resurssin ja sen muokkaamiseen tarvittavat funktiot
 const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([]) // resurssin state, tänne tallennetaan resurssin sisältö ja sen setteri
 
-    const [resources, setResources] = useState([])
-
-
-    const fetchAll = () => { // hakee resurssin
+    useEffect(() => { // hakee resurssin aina, kun baseUrl muuttuu
         axios
-            .get(baseUrl) // baseUrl on parametri, joka saadaan App-komponentista
+            .get(baseUrl)
             .then(response => {
-                setResources(response.data) // asettaa resurssin stateen
+                setResources(response.data)
             })
-    }
-
-    const create = (resource) => { // luo resurssin
-        axios
-            .post(baseUrl, resource)
-            .then(response => {
-                setResources(resources.concat(response.data)) // lisää uuden resurssin vanhojen joukkoon
+            .catch(error => {
+                console.log(error)
             })
-    }
+    }, [baseUrl])
 
-    const service = { // luo objektin, jossa on funktiot
-        create, fetchAll
-    }
+  const create = (resource) => { // luo resurssin, ottaa parametrina resurssin sisällön objektina
+    axios
+      .post(baseUrl, resource)
+      .then(response => {
+        setResources(resources.concat(response.data)) // resursseihin concatataan uusi resurssi ja ne asetetaan stateen
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 
-    return [ // palauttaa resurssin staten ja sen muokkaamiseen tarvittavat funktiot
-        resources, service
-    ]
+  const service = { // objekti, jossa on funktiot
+    create
+  }
+
+  // palauttaa resurssin staten ja sen muokkaamiseen tarvittavat service funktiot
+  return [
+    resources, service
+  ]
 }
 
 const App = () => {
-    const content = useField('text') // käyttää useField-hookia inputteja varten
-    const name = useField('text')
-    const number = useField('text')
+  const content = useField('text')
+  const name = useField('text')
+  const number = useField('text')
 
-    const [notes, noteService] = useResource('http://localhost:3005/notes') // käyttää useResource-hookia resurssien muokkaamiseen
-    const [persons, personService] = useResource('http://localhost:3005/persons')
+  const [notes, noteService] = useResource('http://localhost:3005/notes')
+  const [persons, personService] = useResource('http://localhost:3005/persons')
 
+  const handleNoteSubmit = (event) => {
+    event.preventDefault()
+    noteService.create({ content: content.value })
+  }
+ 
+  const handlePersonSubmit = (event) => {
+    event.preventDefault()
+    personService.create({ name: name.value, number: number.value})
+    console.log('persons', persons)
+  }
 
-    useEffect(() => { // hakee resurssit, kun komponentti renderöidään ensimmäistä kertaa
-        noteService.fetchAll() // noteService on objekti, jossa on funktiot, noudetaan kaikki fetchAll-funktiolla
-        personService.fetchAll()
-    }, [noteService, personService]) // useEffect suoritetaan aina, kun noteService tai personService muuttuu
+  return (
+    <div>
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...content} />
+        <button>create</button>
+      </form>
+      {notes.map(n => <p key={n.id}>{n.content}</p>)}
 
-
-
-    const handleNoteSubmit = (event) => {
-        event.preventDefault()
-        noteService.create({ content: content.value })
-    }
-
-    const handlePersonSubmit = (event) => {
-        event.preventDefault()
-        personService.create({ name: name.value, number: number.value })
-    }
-
-    return (
-        <div>
-            <h2>notes</h2>
-            <form onSubmit={handleNoteSubmit}>
-                <input {...content} />
-                <button>create</button>
-            </form>
-            {notes.map(n => <p key={n.id}>{n.content}</p>)}
-
-            <h2>persons</h2>
-            <form onSubmit={handlePersonSubmit}>
-                name <input {...name} /> <br />
-                number <input {...number} />
-                <button>create</button>
-            </form>
-            {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
-        </div>
-    )
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input {...name} /> <br/>
+        number <input {...number} />
+        <button>create</button>
+      </form>
+      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+    </div>
+  )
 }
 
 export default App
+
+
+
+
+
+
+// import { useState, useEffect } from 'react'
+// import axios from 'axios'
+
+// const useField = (type) => {
+//     const [value, setValue] = useState('')
+
+//     const onChange = (event) => { // asettaa inputin arvon stateen
+//         setValue(event.target.value)
+//     }
+
+//     return { // palauttaa objektin, jossa on value, onChange ja type
+//         type,
+//         value,
+//         onChange
+//     }
+// }
+
+// // custom hook, joka palauttaa resurssin ja sen muokkaamiseen tarvittavat funktiot
+// const useResource = (baseUrl) => {
+
+//     const [resources, setResources] = useState([])
+
+
+//     const fetchAll = () => { // hakee resurssin
+//         axios
+//             .get(baseUrl) // baseUrl on parametri, joka saadaan App-komponentista
+//             .then(response => {
+//                 setResources(response.data) // asettaa resurssin stateen
+//             })
+//             .catch(error => {
+//                 console.log(error)
+//             })
+//     }
+
+//     const create = (resource) => { // luo resurssin
+//         axios
+//             .post(baseUrl, resource)
+//             .then(response => {
+//                 setResources(resources.concat(response.data)) // lisää uuden resurssin vanhojen joukkoon
+//             })
+//             .catch(error => {
+//                 console.log(error)
+//             })
+//     }
+
+//     const service = { // luo objektin, jossa on funktiot
+//         create, fetchAll
+//     }
+
+//     return [ // palauttaa resurssin staten ja sen muokkaamiseen tarvittavat funktiot
+//         resources, service
+//     ]
+// }
+
+// const App = () => {
+//     const content = useField('text') // käyttää useField-hookia inputteja varten
+//     const name = useField('text')
+//     const number = useField('text')
+
+//     const [notes, noteService] = useResource('http://localhost:3005/notes') // käyttää useResource-hookia resurssien muokkaamiseen
+//     const [persons, personService] = useResource('http://localhost:3005/persons')
+
+    
+    
+//     noteService.fetchAll()
+//     personService.fetchAll()
+    
+    
+
+
+//     const handleNoteSubmit = (event) => {
+//         event.preventDefault()
+//         noteService.create({ content: content.value })
+//     }
+
+//     const handlePersonSubmit = (event) => {
+//         event.preventDefault()
+//         personService.create({ name: name.value, number: number.value })
+//     }
+
+//     return (
+//         <div>
+//             <h2>notes</h2>
+//             <form onSubmit={handleNoteSubmit}>
+//                 <input {...content} />
+//                 <button>create</button>
+//             </form>
+//             {notes.map(n => <p key={n.id}>{n.content}</p>)}
+
+//             <h2>persons</h2>
+//             <form onSubmit={handlePersonSubmit}>
+//                 name <input {...name} /> <br />
+//                 number <input {...number} />
+//                 <button>create</button>
+//             </form>
+//             {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+//         </div>
+//     )
+// }
+
+// export default App

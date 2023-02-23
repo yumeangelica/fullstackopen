1724497542b2a57592c785deb4d8b5780b763899
Angelica <angelica@Angelica-Macbook.wlan>
2023-02-23 14:29:41 +0200

    style navbar

--- osa7/bloglist-frontend/src/components/NavigationMenu.js
+++ osa7/bloglist-frontend/src/components/NavigationMenu.js
@@ -1,15 +1,38 @@
 import { Link } from 'react-router-dom'
 
-import { Button } from 'react-bootstrap'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const NavigationMenu = ({ user, handleLogout }) => { // 7.17 navigaatiomenu
-  return (
-    <div>
-        <Link to="/">blogs</Link> &nbsp;
-        <Link to="/users">users</Link> &nbsp;
-        {user.name} logged in <Button variant='dark' onClick={handleLogout}>logout</Button>
-    </div>
-  )
+const navLinkStyle = {
+    fontSize: 20,
+}
+
+
+const NavigationMenu = ({ user, handleLogout }) => { // 7.17 navigaatiomenu, styled and responsive with bootstrap
+    return (
+
+        <Navbar style={navLinkStyle} collapseOnSelect expand="lg" bg="light" variant="light">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#" as="span">
+                        <Link to="/">blogs</Link>
+                    </Nav.Link>
+                    <Nav.Link href="#" as="span">
+                        <Link to="/users">users</Link>
+                    </Nav.Link>
+                    <NavDropdown title={user.name} id="collasible-nav-dropdown">
+                        <NavDropdown.Item href="#" as="span">
+                            <span onClick={handleLogout}>logout</span>
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+
+            </Navbar.Collapse>
+        </Navbar>
+
+    )
 }
 
 export default NavigationMenu
